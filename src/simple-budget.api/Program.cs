d8b82a0263f7581ff756a3741d69002ba372d992
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-09 21:16:45 -0500

    Wire up Serilog/Seq

--- src/simple-budget.api/Program.cs
+++ src/simple-budget.api/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,17 +9,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog((context, configuration) => {
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Error");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{    
+    Serilog.Debugging.SelfLog.Enable(Console.Error);
+    app.Run();
+}
+finally
+{
+    Log.CloseAndFlush();
+}
