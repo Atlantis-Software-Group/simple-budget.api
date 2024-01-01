e5670cfc62f0d135d49cdcc02eaaaa36681d0aa6
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-31 22:25:23 -0500

    Update entrypoint.sh
    update the entrypoint to restore, build and start watch for the API.

--- src/simple-budget.api/entrypoint.sh
+++ src/simple-budget.api/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-update-ca-certificates
+cd src
+cd simple-budget.api
+dotnet restore ./simple-budget.api.csproj
+dotnet build ./simple-budget.api.csproj
 dotnet watch ./bin/Debug/net8.0/simple-budget.api.dll
